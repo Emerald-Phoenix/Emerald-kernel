f2bd7f0dd61a030feb7f80a77928093e0633fce5
Nick <darkbahamut@gmail.com>
2018-05-21 01:08:02 +0100

    Remove some files that were tracked by mistake

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 169 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#169 SMP PREEMPT Sat Mar 24 02:05:48 GMT 2018"
-#define LINUX_COMPILE_BY "darkbahamut"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 6.1.1 20160711 (Linaro GCC Snapshot 6.1-2016.07) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.10.108-g90265f54-dirty"
